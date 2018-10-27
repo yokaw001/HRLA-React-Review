d8788da61ac87e6f25ab7b205d31f3d93e6b5431
brandon <brandonrosenstein@gmail.com>
2018-10-27 11:45:13 -0700

    finished step 2

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,12 +6,18 @@ class App extends React.Component{
     this.state={
       task: ''
     }
+    this.handleText = this.handleText.bind(this);
+  }
+
+  handleText(e){
+    console.log(e.target.value)
   }
 
   render(){
     return(
       <div>
         <h1>Hello from React!</h1>
+        <input type='text' onChange={this.handleText}></input>
       </div>
     )
   }
