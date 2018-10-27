f6489badace804ed60fc1ad5491d487ef1e2807a
brandon <brandonrosenstein@gmail.com>
2018-10-27 11:34:15 -0700

    base level

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,2 @@
 //import react dom and react from here
 //implement render file here
-import React from 'react';
-import reactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-reactDOM.render(<App /> ,document.getElementById('root'))
